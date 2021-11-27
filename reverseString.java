fedd702cb1ef3719e50dba63f01d20e7a996668e
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-11-27 18:29:19 +0530

    Add files via upload

---
+++ reverseString.java
@@ -0,0 +1,13 @@
+
+public class reverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String name ="senthil";
+     
+      for(int i=name.length()-1;i>=0;i--) {
+    	  System.out.print(name.charAt(i));
+      }
+	}
+
+}
