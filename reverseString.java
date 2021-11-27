c9ad64bb2546852f15b0537e33be5a10a45d8edd
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-11-27 18:32:02 +0530

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -3,11 +3,11 @@ public class reverseString {
 
 	public static void main(String[] args) {
 		
-      String name ="senthil";
+        String name ="senthil";
      
-      for(int i=name.length()-1;i>=0;i--) {
-    	  System.out.print(name.charAt(i));
+         for(int i=name.length()-1;i>=0;i--) {
+     	    System.out.print(name.charAt(i));
       }
-	}
+ }
 
 }
